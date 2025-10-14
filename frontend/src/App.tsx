58fc904d3c79c7f75d8125b6cd3e0b23cdd1f9f0
ozzyozbourne <khanosaid726@gmail.com>
2025-10-13 21:28:55 -0400

    checking prod

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,12 @@ import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
-  return (
-    <MainLayout>
-      <HomePage />
-    </MainLayout>
-  );
+    return (
+        <MainLayout>
+            <HomePage />
+        </MainLayout>
+    );
 }
 
 export default App;
+
