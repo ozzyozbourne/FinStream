45c1c96c8d3a26a1c31f105e608af344f7acbc35
ahmedz17 <ahmedzoha12@gmail.com>
2025-10-19 19:55:51 -0400

    authentication+profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
 import HomePage from './pages/HomePage';
+import ProfilePage from './pages/Profile/ProfilePage';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <MainLayout>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </MainLayout>
+  </Router>
   );
 }
 
